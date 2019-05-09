c1efc0e77a1e116675e6f535ad715b99ae23e7df
pkurjanowicz <pkurjanowicz5@gmail.com>
2019-05-09 17:16:12 -0600

    Update sort_contacts.py

--- sort_contacts.py
+++ sort_contacts.py
@@ -1,3 +1,13 @@
-def sort_contacts(param):
-    # code here
-    return ""
+def sort_contacts(dictionary):
+    new_list = []
+    for key in dictionary:
+        sub_list_tuple = ()
+        sub_list_list = list(sub_list_tuple)
+        var1, var2 = dictionary[key]
+        sub_list_list.append(key)
+        sub_list_list.append(var1)
+        sub_list_list.append(var2)
+        sub_list_tuple = tuple(sub_list_list)
+        new_list.append(sub_list_tuple)
+    new_list.sort()
+    return new_list
